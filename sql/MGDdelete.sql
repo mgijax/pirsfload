172a61a87798d821ea9d8646ab83a20bcb3346b1
mbw <>
2005-10-06 20:30:23 +0000

    corrected sql case

--- sql/MGDdelete.sql
+++ sql/MGDdelete.sql
@@ -1,16 +1,16 @@
 declare @annottypekey integer
-select @annottypekey = _annottype_key from VOC_AnnotType where name = 'PIRSF/Marker'
+select @annottypekey = _AnnotType_key from VOC_AnnotType where name = 'PIRSF/Marker'
 
 declare @vocabkey integer
-select @vocabkey = _vocab_key from voc_vocab where name = 'PIR Superfamily'
+select @vocabkey = _Vocab_key from VOC_Vocab where name = 'PIR Superfamily'
 
-delete from voc_evidence
-from voc_evidence e, voc_annot a
-where a._annottype_key = @annottypekey
-and a._annot_key = e._annot_key
+delete from VOC_Evidence
+from VOC_Evidence e, VOC_Annot a
+where a._AnnotType_key = @annottypekey
+and a._Annot_key = e._Annot_key
 
-delete from voc_annot
-where _annottype_key = @annottypekey
+delete from VOC_Annot
+where _AnnotType_key = @annottypekey
 
-delete from voc_term 
-where _vocab_key = @vocabkey
+delete from VOC_Term 
+where _Vocab_key = @vocabkey
